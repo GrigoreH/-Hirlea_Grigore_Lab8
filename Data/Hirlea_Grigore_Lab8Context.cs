a2a7e1068c8c87b41ef3e1dce674e6a21af3af5c
Grigore Hirlea <grigore.hirlea@evozon.com>
2021-12-31 13:49:47 +0200

    lab8

---
+++ Data/Hirlea_Grigore_Lab8Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hirlea_Grigore_Lab8.Models;
+
+namespace Hirlea_Grigore_Lab8.Data
+{
+    public class Hirlea_Grigore_Lab8Context : DbContext
+    {
+        public Hirlea_Grigore_Lab8Context (DbContextOptions<Hirlea_Grigore_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hirlea_Grigore_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Hirlea_Grigore_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Hirlea_Grigore_Lab8.Models.Category> Category { get; set; }
+
+        public DbSet<Hirlea_Grigore_Lab8.Models.BookCategory> BookCategory { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Hirlea_Grigore_Lab8.Data;
+using Hirlea_Grigore_Lab8.Models;
+
+namespace Hirlea_Grigore_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Hirlea_Grigore_Lab8.Data.Hirlea_Grigore_Lab8Context _context;
+
+        public IndexModel(Hirlea_Grigore_Lab8.Data.Hirlea_Grigore_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
